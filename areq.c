84028d4ced4de87fab5c34c7d1b94c8649e179a8
cse533-7 Student <cse533-7@minix.cs.sunysb.edu>
2015-12-12 00:28:15 -0500

    API

--- areq.c
+++ areq.c
@@ -13,7 +13,7 @@ areq (struct in_addr *IPaddr, socklen_t sockaddrlen, struct hwaddr *HWaddr){
 	timeout.tv_sec = TIMEOUT;
 	int n, i;
 	
-	sockfd = Socket(AF_LOCAL, SOCK_STREAM, 0);
+	sockfd = Socket(AF_LOCAL, SOCK_DGRAM, 0);
 	Setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
 
 	init_sockaddr_un(&addr, SUN_PATH);
@@ -27,12 +27,12 @@ areq (struct in_addr *IPaddr, socklen_t sockaddrlen, struct hwaddr *HWaddr){
 	
 	memcpy(&ar.dest_hw, HWaddr, sizeof(struct hwaddr));
 
-	HWaddr->sll_addr[0]= 0x00;
+	/*HWaddr->sll_addr[0]= 0x00;
 	HWaddr->sll_addr[1]= 0x0c;
 	HWaddr->sll_addr[2]= 0x29;
 	HWaddr->sll_addr[3]= 0x49;
 	HWaddr->sll_addr[4]= 0x3f;
-	HWaddr->sll_addr[5]= 0x5b;
+	HWaddr->sll_addr[5]= 0x5b;*/
 
 	printf("Requesting HW address of %s \t\n", inet_ntoa(ar.dest_ip));
 	
@@ -43,15 +43,15 @@ areq (struct in_addr *IPaddr, socklen_t sockaddrlen, struct hwaddr *HWaddr){
 
 	/* Set timeout of 5 secs for the socket */	
 	/*if (setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
-		err_quit("setsockopt failed\n");
+		err_quit("setsockopt failed\n");*/
 	
-	Send(sockfd, &ar, sizeof(ar), 0);
+	Sendto(sockfd, &ar, sizeof(ar), 0, (SA *)&addr, sizeof(addr));
 
-	n = recv(sockfd, HWaddr, sizeof(HWaddr), 0 );
+	n = recvfrom(sockfd, &ar, sizeof(HWaddr), 0, (SA *)&addr, sizeof(addr) );
 	if((n < 0) || (n == EWOULDBLOCK))
 		return -1;
 	
-	close(sockfd);*/
+	close(sockfd);
 	return sizeof(HWaddr); 
 		
 }
